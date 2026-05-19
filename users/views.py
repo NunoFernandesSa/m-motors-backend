0727b233d73a3ed067babb828cd03e88206c6743
NunoF <n.fernandes.contact@gmail.com>
2026-05-20 01:08:11 +0200

    Add user registration and detail retrieval views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+# Register API view
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+
+# endpoint to get the current authenticated user's details
+class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
