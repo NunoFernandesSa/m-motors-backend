0c720e93fc9468907de38ebc12300d11fdf56f98
NunoF <n.fernandes.contact@gmail.com>
2026-05-20 01:14:11 +0200

    Add user authentication endpoints for registration and detail retrieval

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from users.views import RegisterView, UserDetailView
+
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/users/register/', RegisterView.as_view(), name='user_register'),
+    path('api/users/detail/', UserDetailView.as_view(), name='user_detail'),
 ]
