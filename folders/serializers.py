46e6ca4dd0b43d12c65f59eeb9ba9aaae14b6db3
NunoF <n.fernandes.contact@gmail.com>
2026-05-23 04:33:45 +0200

    Add DocumentSerializer to handle document data in folder management

--- folders/serializers.py
+++ folders/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Folder
+from .models import Document, Folder
 from vehicles.serializers import VehicleSerializer
 from users.serializers import UserSerializer
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Document model, including the file field and the upload timestamp.
+    """
+
+    class Meta:
+        model = Document
+        fields = ['id', 'file', 'uploaded_at']
 
 
 class FolderSerializer(serializers.ModelSerializer):
@@ -11,6 +21,7 @@ class FolderSerializer(serializers.ModelSerializer):
     
     user_details = UserSerializer(source='user', read_only=True)
     vehicle_details = VehicleSerializer(source='vehicle', read_only=True)
+    documents = DocumentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Folder
