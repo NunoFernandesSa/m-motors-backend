7b7a715907020f146684d3eac8ebb0c58a617329
NunoF <n.fernandes.contact@gmail.com>
2026-05-23 02:10:09 +0200

    Update authentication URL path to improve API structure

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/users/', include('users.urls')),
+    path('api/auth/', include('users.urls')),
 ]
