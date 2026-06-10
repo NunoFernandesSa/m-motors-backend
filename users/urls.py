678a297709ffd5cdc92884c4e784e2678013c046
NunoF <n.fernandes.contact@gmail.com>
2026-06-10 21:11:35 +0200

    feat(password-reset): update password reset URLs and add email template

--- users/urls.py
+++ users/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('me/', UserDetailView.as_view(), name='user_detail'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('auth/password/reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('auth/password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password/reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
