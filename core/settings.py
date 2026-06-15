b4d17f0c98c7038fa7ee65dfe0f0eab7a1bc8d2c
NunoF <n.fernandes.contact@gmail.com>
2026-06-15 03:04:13 +0200

    feat(auth): add CookieJWTAuthentication for token retrieval from cookies

--- core/settings.py
+++ core/settings.py
@@ -159,6 +159,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "users.authentication.CookieJWTAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
